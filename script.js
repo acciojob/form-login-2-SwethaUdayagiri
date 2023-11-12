a0f23ee92fba6262faad7925b3d1d4de21b1b0ea
Swetha Udayagiri <106729744+SwethaUdayagiri@users.noreply.github.com>
2023-11-12 16:46:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+alertbox1(){
+	var fName=document.getElementByName('First Name').value;
+	var LName=document.getElementByName('Last Name').value;
+	var phone=document.getElementByName('Phone Number').value;
+	var emailId=document.getElementByName('Email ID').value;
+	alert("First Name: "+fName+"\nLast Name: "+LName
+		  +"\nPhone Number: "+phone+"\nEmail ID: "+emailId);
+}
